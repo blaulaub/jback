d58146f6df23486b400351988c01455ae59844b4
Christian Fuchs <cfuchs@scs.ch>
2020-08-21 14:31:02 +0200

    add HelloWorld controller

--- jback-main/src/main/java/ch/patchcode/jback/main/Main.java
+++ jback-main/src/main/java/ch/patchcode/jback/main/Main.java
@@ -1,12 +1,17 @@
 package ch.patchcode.jback.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"ch.patchcode.jback.main", "ch.patchcode.jback.api"})
 public class Main {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
     }
 }
