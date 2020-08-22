449ea020376be09ecf0e087798efa4cc786b5751
Christian Fuchs <cfuchs@patchcode.ch>
2020-08-22 14:39:40 +0200

    first api mockmvc test (stub)

--- jback-api/src/main/java/ch/patchcode/jback/api/persons/PersonsController.java
+++ jback-api/src/main/java/ch/patchcode/jback/api/persons/PersonsController.java
@@ -1,11 +1,13 @@
 package ch.patchcode.jback.api.persons;
 
 import ch.patchcode.jback.core.persons.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -14,9 +16,16 @@ public class PersonsController {
 
     private PersonService personService;
 
+    @Autowired
+    public PersonsController(PersonService personService) {
+
+        this.personService = personService;
+    }
+
     @GetMapping("{id}")
     public Person getPerson(@PathVariable("id") UUID id) {
 
-        return Person.from(personService.getPerson(id));
+        // TODO null? rather 404
+        return Optional.ofNullable(personService.getPerson(id)).map(Person::from).orElse(null);
     }
 }
--- jback-api/src/test/java/ch/patchcode/jback/api/persons/PersonsControllerTest.java
+++ jback-api/src/test/java/ch/patchcode/jback/api/persons/PersonsControllerTest.java
@@ -2,16 +2,30 @@ package ch.patchcode.jback.api.persons;
 
 import ch.patchcode.jback.api.SpringTestConfiguration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PersonsController.class)
 @ContextConfiguration(classes = {SpringTestConfiguration.class})
+//@AutoConfigureMockMvc
 class PersonsControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void test() {
+    void test() throws Exception {
+        var id = UUID.randomUUID();
+        mvc.perform(get("/persons/{id}", id))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
