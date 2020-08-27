d71090f1a6b92975c8cdac3a49d25bcd70f44ee9
Christian Fuchs <cfuchs@patchcode.ch>
2020-08-27 19:12:13 +0200

    clarify ConfirmationResult

--- jback-core/src/main/java/ch/patchcode/jback/core/registration/ConfirmationResult.java
+++ jback-core/src/main/java/ch/patchcode/jback/core/registration/ConfirmationResult.java
@@ -3,6 +3,10 @@ package ch.patchcode.jback.core.registration;
 public enum ConfirmationResult {
 
     SUCCESS,
+
+    // implies no retry
     NOT_FOUND,
+
+    // implies retry
     WRONG_CODE;
 }
