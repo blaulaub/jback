78be9d7b368b67fc5486cd4ecf967a270ae357bc
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-02 23:41:27 +0200

    disable csrf check for testing

--- jback-api/src/test/java/ch/patchcode/jback/api/ApiWebSecurityTestConfig.java
+++ jback-api/src/test/java/ch/patchcode/jback/api/ApiWebSecurityTestConfig.java
@@ -1,17 +1,17 @@
 package ch.patchcode.jback.api;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityTestConfig extends WebSecurityConfigurerAdapter {
+public class ApiWebSecurityTestConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        // disable any security checks
-        web.ignoring().antMatchers("/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
     }
 }
