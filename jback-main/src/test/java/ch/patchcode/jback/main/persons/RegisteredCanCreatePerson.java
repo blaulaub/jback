fab2c296cb1c2d79bde90f6f425be015c6e89431
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-06 15:54:03 +0200

    test creating a person

--- jback-main/src/test/java/ch/patchcode/jback/main/persons/RegisteredCanCreatePerson.java
+++ jback-main/src/test/java/ch/patchcode/jback/main/persons/RegisteredCanCreatePerson.java
@@ -1,7 +1,9 @@
 package ch.patchcode.jback.main.persons;
 
 import ch.patchcode.jback.api.persons.Person;
+import ch.patchcode.jback.api.registration.VerificationCode;
 import ch.patchcode.jback.main.MainTestConfiguration;
+import ch.patchcode.jback.main.fakes.FixVerificationCodeProvider;
 import ch.patchcode.jback.main.restApi.RestApi;
 import ch.patchcode.jback.main.util.RestSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,31 +14,43 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static ch.patchcode.jback.main.util.SomeData.someInitialRegistrationData;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = {MainTestConfiguration.class})
-public class NotAnybodyCanCreatePersons {
+public class RegisteredCanCreatePerson {
 
     private final RestApi api;
 
     @Autowired
-    public NotAnybodyCanCreatePersons(@LocalServerPort int port, TestRestTemplate restTemplate, ObjectMapper objectMapper) {
+    public RegisteredCanCreatePerson(@LocalServerPort int port, TestRestTemplate restTemplate, ObjectMapper objectMapper) {
         this.api = new RestApi(new RestSession(port, restTemplate, objectMapper));
     }
 
     @Test
-    void withoutAuthorization_creatingPerson_fails() throws Exception {
+    void afterRegistration_creatingPerson_succeeds() throws Exception {
 
         Person.Draft newPerson = new Person.Draft.Builder()
                 .setFirstName("Tom")
                 .setLastName("Sawyer")
                 .build();
 
-        assertThrows(RestSession.ForbiddenException.class, () -> {
-            api.personsPostNewPerson(newPerson)
-                    .checkResultIsSuccess()
-                    .andReturnBody();
-        });
+        // post registration
+        var pendingRegistration = api.registrationPostData(someInitialRegistrationData())
+                .checkResultIsSuccess()
+                .andReturnBody();
+
+        // put code
+        api.registrationPutCode(
+                pendingRegistration,
+                VerificationCode.of(FixVerificationCodeProvider.FIX_VERIFICATION_CODE))
+                .checkResultIsSuccess();
+
+        // create person
+        var createdPerson = api.personsPostNewPerson(newPerson)
+                .checkResultIsSuccess()
+                .andReturnBody();
+
     }
+
 }
