071332e5352f10d30a820d42f0357f2e05de2893
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-07 00:29:32 +0200

    let (secBase) principal have identification means

--- jback-sec-base/src/main/java/ch/patchcode/jback/secBase/secModelImpl/Principal.java
+++ jback-sec-base/src/main/java/ch/patchcode/jback/secBase/secModelImpl/Principal.java
@@ -1,4 +1,13 @@
 package ch.patchcode.jback.secBase.secModelImpl;
 
+import ch.patchcode.jback.secBase.VerificationMean;
+
+import java.util.List;
+
 public interface Principal extends ch.patchcode.jback.secModel.Principal<Organisation, Person, Principal, Authority, Role, User> {
+
+    /**
+     * List of means the principal can be verified by.
+     */
+    List<VerificationMean> getMeans();
 }
--- jback-security/src/main/java/ch/patchcode/jback/security/TemporaryAuthentication.java
+++ jback-security/src/main/java/ch/patchcode/jback/security/TemporaryAuthentication.java
@@ -1,6 +1,7 @@
 package ch.patchcode.jback.security;
 
 import ch.patchcode.jback.secBase.PendingRegistration;
+import ch.patchcode.jback.secBase.VerificationMean;
 import ch.patchcode.jback.secBase.secModelImpl.Authority;
 import ch.patchcode.jback.security.authorities.ApiAuthority;
 import org.springframework.security.core.Authentication;
@@ -9,20 +10,26 @@ import org.springframework.security.core.GrantedAuthority;
 import java.util.Collection;
 import java.util.List;
 
-import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
+/**
+ * This is the most stupid form of an authenticated user (beside the anonymous user). What we have here is
+ * someone who can verify his identity via something given by {@link #getMeans()}.
+ */
 public class TemporaryAuthentication implements Authentication, ch.patchcode.jback.secBase.secModelImpl.Principal {
 
     private final String principal;
     private final String code;
     private final String userName;
+    private final VerificationMean mean;
 
     public TemporaryAuthentication(PendingRegistration.Id id, PendingRegistration registration) {
 
         this.principal = id.getId().toString();
         this.code = registration.getVerificationCode();
         this.userName = String.join(" ", registration.getFirstName(), registration.getLastName());
+        this.mean = registration.getVerificationMean();
     }
 
     // impl org.springframework.security.core.Authentication
@@ -74,8 +81,14 @@ public class TemporaryAuthentication implements Authentication, ch.patchcode.jba
     // impl ch.patchcode.jback.secBase.secModelImpl.Principal
 
     @Override
+    public List<VerificationMean> getMeans() {
+
+        return singletonList(this.mean);
+    }
+
+    @Override
     public List<Authority> getBasicPrivileges() {
 
-        return asList(Authority.CAN_CREATE_PERSON);
+        return singletonList(Authority.CAN_CREATE_PERSON);
     }
 }
