d439118a9e300be61a7f260d2ba6af4c1734a38b
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-08 00:13:35 +0200

    explicit overrides

--- jback-core/src/main/java/ch/patchcode/jback/core/persons/Person.java
+++ jback-core/src/main/java/ch/patchcode/jback/core/persons/Person.java
@@ -1,8 +1,12 @@
 package ch.patchcode.jback.core.persons;
 
 import ch.patchcode.jback.core.common.Address;
+import ch.patchcode.jback.secBase.secModelImpl.Authority;
+import ch.patchcode.jback.secBase.secModelImpl.Principal;
+import ch.patchcode.jback.secBase.secModelImpl.Role;
 import org.inferred.freebuilder.FreeBuilder;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,6 +20,17 @@ public interface Person extends ch.patchcode.jback.secBase.secModelImpl.Person {
     String getLastName();
 
     Optional<Address> getAddress();
+
+    // from secModel Person
+
+    @Override
+    List<Principal> getPrincipals();
+
+    @Override
+    List<Role> getRoles();
+
+    @Override
+    List<Authority> getExtraPrivileges();
 
     class Builder extends Person_Builder {
     }
