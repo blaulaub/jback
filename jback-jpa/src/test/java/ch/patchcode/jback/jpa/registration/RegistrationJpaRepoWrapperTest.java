7cc6c58806e78d229e06ca3ae20ea74020d3f798
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-09 19:39:00 +0200

    test person jpa repo wrapper end-to-end (incl real jpa repo)

--- jback-jpa/src/test/java/ch/patchcode/jback/jpa/registration/RegistrationJpaRepoWrapperTest.java
+++ jback-jpa/src/test/java/ch/patchcode/jback/jpa/registration/RegistrationJpaRepoWrapperTest.java
@@ -1,71 +1,44 @@
 package ch.patchcode.jback.jpa.registration;
 
-import ch.patchcode.jback.jpa.util.SomeData;
+import ch.patchcode.jback.jpa.JpaTestConfiguration;
 import ch.patchcode.jback.secBase.PendingRegistration;
 import ch.patchcode.jback.secBase.VerificationMean;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import java.util.Optional;
-import java.util.UUID;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static ch.patchcode.jback.jpa.util.SomeData.somePendingRegistration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {JpaTestConfiguration.class})
 class RegistrationJpaRepoWrapperTest {
 
-    @Mock
-    private RegistrationJpaRepository registrationJpaRepository;
+    private final RegistrationJpaRepoWrapper wrapper;
 
-    @InjectMocks
-    private RegistrationJpaRepoWrapper wrapper;
-
-    @BeforeEach
-    void setUp() {
-
-        initMocks(this);
+    @Autowired
+    public RegistrationJpaRepoWrapperTest(RegistrationJpaRepoWrapper wrapper) {
+        this.wrapper = wrapper;
     }
 
     @Test
-    void save() {
+    void save_and_findById() {
 
         // arrange
-        PendingRegistration pendingRegistration = somePendingRegistration(new VerificationMean.VerificationByConsole());
-        var id = UUID.randomUUID();
-        var registration = mock(Registration.class);
-        when(registration.getId()).thenReturn(id);
-        when(registrationJpaRepository.save(any())).thenReturn(registration);
+        PendingRegistration pending = somePendingRegistration(new VerificationMean.VerificationByConsole());
 
         // act
-        var result = wrapper.save(pendingRegistration);
-
-        // assert
-        verify(registrationJpaRepository, times(1)).save(any());
-        assertEquals(id, result.getId());
-    }
-
-    @Test
-    void findById() {
-
-        // arrange
-        var id = UUID.randomUUID();
-        Registration registration = SomeData.someRegistration();
-        when(registrationJpaRepository.findById(any())).thenReturn(Optional.of(registration));
-
-        // act
+        var id = wrapper.save(pending).getId();
         var result = wrapper.findById(id);
 
         // assert
         assertTrue(result.isPresent());
-        assertEquals(registration.getFirstName(), result.get().getFirstName());
-        assertEquals(registration.getLastName(), result.get().getLastName());
-        assertEquals(registration.getVerificationCode(), result.get().getVerificationCode());
-        assertEquals(registration.getExpiresAt(), result.get().getExpiresAt().toEpochMilli());
+        assertEquals(pending.getFirstName(), result.get().getFirstName());
+        assertEquals(pending.getLastName(), result.get().getLastName());
+        assertEquals(pending.getVerificationCode(), result.get().getVerificationCode());
+        assertEquals(pending.getExpiresAt().toEpochMilli(), result.get().getExpiresAt().toEpochMilli());
     }
 }
