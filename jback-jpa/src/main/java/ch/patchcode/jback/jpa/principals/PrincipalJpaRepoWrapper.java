8eef94078468338c59a2dc431c5a211d989adb51
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-12 17:14:08 +0200

    let PrincipalJpaRepoWrapper map the means

--- jback-jpa/src/main/java/ch/patchcode/jback/jpa/principals/PrincipalJpaRepoWrapper.java
+++ jback-jpa/src/main/java/ch/patchcode/jback/jpa/principals/PrincipalJpaRepoWrapper.java
@@ -1,6 +1,9 @@
 package ch.patchcode.jback.jpa.principals;
 
 import ch.patchcode.jback.jpa.persons.PersonJpa;
+import ch.patchcode.jback.jpa.registration.ToRegistrationConverter;
+import ch.patchcode.jback.jpa.verificationMeans.VerificationMeanJpa;
+import ch.patchcode.jback.jpa.verificationMeans.VerificationMeanJpaRepository;
 import ch.patchcode.jback.secBase.VerificationMean;
 import ch.patchcode.jback.security.authentications.PersonalAuthentication;
 import ch.patchcode.jback.security.authentications.PersonalAuthenticationRepository;
@@ -10,16 +13,22 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class PrincipalJpaRepoWrapper implements PersonalAuthenticationRepository {
 
     private final PrincipalJpaRepository principalJpaRepository;
+    private final VerificationMeanJpaRepository verificationMeanJpaRepository;
 
     @Autowired
-    public PrincipalJpaRepoWrapper(PrincipalJpaRepository principalJpaRepository) {
+    public PrincipalJpaRepoWrapper(
+            PrincipalJpaRepository principalJpaRepository,
+            VerificationMeanJpaRepository verificationMeanJpaRepository
+    ) {
 
         this.principalJpaRepository = principalJpaRepository;
+        this.verificationMeanJpaRepository = verificationMeanJpaRepository;
     }
 
     @Override
@@ -27,8 +36,52 @@ public class PrincipalJpaRepoWrapper implements PersonalAuthenticationRepository
 
         PrincipalJpa principal = persist(personalAuthentication);
 
-        // TODO instead of emptyList() there should be the list of the true means instead
-        List<VerificationMean> means = emptyList();
+        List<VerificationMean> means = personalAuthentication.getMeans().stream()
+                .map(it -> it.accept(new VerificationMean.VerificationMeanVisitor<VerificationMeanJpa>() {
+                    @Override
+                    public VerificationMeanJpa visit(VerificationMean.VerificationByConsole verificationByConsole) {
+                        VerificationMeanJpa.ConsoleVerification consoleVerification = new VerificationMeanJpa.ConsoleVerification();
+                        consoleVerification.setPrincipal(principal);
+                        return consoleVerification;
+                    }
+
+                    @Override
+                    public VerificationMeanJpa visit(VerificationMean.VerificationByEmail verificationByEmail) {
+                        VerificationMeanJpa.EmailVerification emailVerification = new VerificationMeanJpa.EmailVerification();
+                        emailVerification.setPrincipal(principal);
+                        emailVerification.setEmail(verificationByEmail.getEmailAddress());
+                        return emailVerification;
+                    }
+
+                    @Override
+                    public VerificationMeanJpa visit(VerificationMean.VerificationBySms verificationBySms) {
+                        VerificationMeanJpa.SmsVerification smsVerification = new VerificationMeanJpa.SmsVerification();
+                        smsVerification.setPrincipal(principal);
+                        smsVerification.setPhoneNumber(verificationBySms.getPhoneNumber());
+                        return smsVerification;
+                    }
+                }))
+                .map(verificationMeanJpaRepository::save)
+                .map(it -> it.accept(new VerificationMeanJpa.Visitor<VerificationMean>() {
+                    @Override
+                    public VerificationMean visit(VerificationMeanJpa.ConsoleVerification consoleVerification) {
+                        return new VerificationMean.VerificationByConsole();
+                    }
+
+                    @Override
+                    public VerificationMean visit(VerificationMeanJpa.EmailVerification emailVerification) {
+                        return new VerificationMean.VerificationByEmail.Builder()
+                                .setEmailAddress(emailVerification.getEmail())
+                                .build();
+                    }
+
+                    @Override
+                    public VerificationMean visit(VerificationMeanJpa.SmsVerification smsVerification) {
+                        return new VerificationMean.VerificationBySms.Builder()
+                                .setPhoneNumber(smsVerification.getPhoneNumber())
+                                .build();
+                    }
+                })).collect(toList());
 
         return new PersonalAuthentication(
                 principal.getSelf().toDomain(),
