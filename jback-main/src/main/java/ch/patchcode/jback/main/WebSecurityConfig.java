d10556b22717cc6d2cbbbec05940e794f0907b04
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-14 18:42:46 +0200

    start delivering SPA and API

---
+++ jback-main/src/main/java/ch/patchcode/jback/main/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package ch.patchcode.jback.main;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // who can access what (the order here is important)
+        http.authorizeRequests()
+                // API registration is open to anybody
+                .antMatchers("/api/v1/registration").permitAll()
+                // API is open to authenticated (only)
+                .antMatchers("/api/v1/**").authenticated()
+                .antMatchers("/api/v1/**").denyAll()
+                // swagger is open (that looks like wide open)
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/webjars/springfox-swagger-ui/**").permitAll()
+                .antMatchers("/v2/api-docs/**").permitAll()
+                // otherwise the SPA takes over
+                .anyRequest().permitAll();
+
+
+        // how to login
+        http.formLogin().disable();
+    }
+}
