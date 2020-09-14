65ef9983fd3e327deb13166d78cbc401af422839
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-14 19:15:36 +0200

    add own org.springframework.boot.web.servlet.error.ErrorController

--- jback-main/src/main/java/ch/patchcode/jback/main/Main.java
+++ jback-main/src/main/java/ch/patchcode/jback/main/Main.java
@@ -6,6 +6,7 @@ import ch.patchcode.jback.jpa.JpaConfiguration;
 import ch.patchcode.jback.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
