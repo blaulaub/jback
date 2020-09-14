7cb43a6a45f32d380aa610158841b6c8423c329b
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-14 17:43:35 +0200

    add a static /register page

--- jback-angular/src/main/ngapp/src/app/app.module.ts
+++ jback-angular/src/main/ngapp/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
