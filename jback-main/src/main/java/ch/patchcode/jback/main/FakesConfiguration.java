41ca3765d1076c8cd93b0c98f5e65052273ffde2
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-14 16:07:41 +0200

    have main runnable (with some Fakes)

---
+++ jback-main/src/main/java/ch/patchcode/jback/main/FakesConfiguration.java
@@ -0,0 +1,22 @@
+package ch.patchcode.jback.main;
+
+import ch.patchcode.jback.secBase.PendingRegistration;
+import ch.patchcode.jback.security.registration.VerificationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FakesConfiguration {
+
+    @Bean
+    VerificationService.EmailVerificationService getEmailVerificationService(VerificationService.ConsoleVerificationService fillIn) {
+
+        return fillIn::sendOut;
+    }
+
+    @Bean
+    VerificationService.SmsVerificationService getSmsVerificationService(VerificationService.ConsoleVerificationService fillIn) {
+
+        return fillIn::sendOut;
+    }
+}
