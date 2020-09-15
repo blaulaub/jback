302211c9e9c6762d02a86b77150b8485a5ba93c6
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-15 20:59:46 +0200

    delegate verification to model

--- jback-angular/src/main/ngapp/src/app/initial-registration-data.ts
+++ jback-angular/src/main/ngapp/src/app/initial-registration-data.ts
@@ -1,7 +1,21 @@
 import { VerificationMean } from './verification-mean';
 
 export class InitialRegistrationData {
+
   firstName = null;
+
   lastName = null;
-  verificationMean = null;
+
+  verificationMean: VerificationMean = null;
+
+  private notNullOrEmpty(str: string): boolean {
+    return typeof str === "string" && str !== "";
+  }
+
+  isValid(): boolean {
+    return this.notNullOrEmpty(this.firstName) &&
+      this.notNullOrEmpty(this.lastName) &&
+      this.verificationMean instanceof VerificationMean &&
+      this.verificationMean.isValid();
+  }
 }
--- jback-angular/src/main/ngapp/src/app/register/register.component.ts
+++ jback-angular/src/main/ngapp/src/app/register/register.component.ts
@@ -15,9 +15,6 @@ export class RegisterComponent implements OnInit {
   readonly methodOptions = [ new VerificationByEmail(), new VerificationBySms() ];
 
   data = new InitialRegistrationData();
-  verificationMethod: VerificationMean;
-  emailAddress: string;
-  phoneNumber: string;
 
   constructor() { }
 
@@ -25,21 +22,14 @@ export class RegisterComponent implements OnInit {
   }
 
   isSms() : boolean {
-    return this.verificationMethod instanceof VerificationBySms;
+    return this.data.verificationMean instanceof VerificationBySms;
   }
 
   isEmail() : boolean {
-    return this.verificationMethod instanceof VerificationByEmail;
-  }
-
-  private notNullOrEmpty(str: string): boolean {
-    return typeof str === "string" && str !== "";
+    return this.data.verificationMean instanceof VerificationByEmail;
   }
 
   canSubmitAndContinue(): boolean {
-    return this.notNullOrEmpty(this.data.firstName) &&
-      this.notNullOrEmpty(this.data.lastName) &&
-      this.verificationMethod.isValid();
+    return this.data.isValid();
   }
-
 }
