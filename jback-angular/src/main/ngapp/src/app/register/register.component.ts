2bdae99072ed5f18d58dcb08212da169049bec23
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-15 19:32:37 +0200

    enable submit only when input seems good

--- jback-angular/src/main/ngapp/src/app/register/register.component.ts
+++ jback-angular/src/main/ngapp/src/app/register/register.component.ts
@@ -11,7 +11,7 @@ export class RegisterComponent implements OnInit {
 
   firstName: string;
   lastName: string;
-  method: string;
+  verificationMethod: string;
   emailAddress: string;
   phoneNumber: string;
 
@@ -20,8 +20,21 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private notNullOrEmpty(str: string): boolean {
+    return typeof str === "string" && str !== "";
+  }
+
+  private verificationMethodValid() : boolean {
+    return this.notNullOrEmpty(this.verificationMethod) && (
+        this.verificationMethod === "SMS" && this.notNullOrEmpty(this.phoneNumber) ||
+        this.verificationMethod === "Email" && this.notNullOrEmpty(this.emailAddress)
+      );
+  }
+
   canSubmitAndContinue(): boolean {
-    return true;
+    return this.notNullOrEmpty(this.firstName) &&
+      this.notNullOrEmpty(this.lastName) &&
+      this.verificationMethodValid();
   }
 
 }
