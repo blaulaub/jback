fca48d7a817d2058df1fe5648a52ee203043137e
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-15 18:39:08 +0200

    populate registration method options from code

--- jback-angular/src/main/ngapp/src/app/register/register.component.ts
+++ jback-angular/src/main/ngapp/src/app/register/register.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  methodOptions = [ "Email", "SMS" ];
+
   method: string;
 
   constructor() { }
