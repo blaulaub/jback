a544c7dc985e2c600d08d5848db7d169c5e69fcf
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-17 19:04:03 +0200

    add Perspective concept

---
+++ jback-sec-base/src/main/java/ch/patchcode/jback/secBase/Perspective.java
@@ -0,0 +1,13 @@
+package ch.patchcode.jback.secBase;
+
+/**
+ * Some distinct perspective on the app domain.
+ * <p>
+ * A perspective determines what a user gets presented in the app domain.
+ * <p>
+ * A person may play several roles, but while using the app, it will
+ * usually have to pick a distinct role, and thus gain a distinct
+ * perspective.
+ */
+public interface Perspective {
+}
