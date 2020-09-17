0dd1ccabceb737b4ece1c15ae071726c8620018f
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-17 19:06:26 +0200

    let role have a perspective

--- jback-sec-base/src/main/java/ch/patchcode/jback/secBase/secModelImpl/Role.java
+++ jback-sec-base/src/main/java/ch/patchcode/jback/secBase/secModelImpl/Role.java
@@ -1,8 +1,12 @@
 package ch.patchcode.jback.secBase.secModelImpl;
 
+import ch.patchcode.jback.secBase.Perspective;
+
 import java.util.List;
 
 public interface Role extends ch.patchcode.jback.secModel.Role<Organisation, Person, Principal, Authority, Role, User>{
+
+    Perspective getPerspective();
 
     // from secModel.Principal
 
