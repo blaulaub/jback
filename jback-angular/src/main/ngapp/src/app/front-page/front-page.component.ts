ae2fe290c0513b58890ec9f6b61a9071b8f8dabc
Christian Fuchs <cfuchs@patchcode.ch>
2020-09-20 10:52:28 +0200

    dynamic front page (depending on perspective)

--- jback-angular/src/main/ngapp/src/app/front-page/front-page.component.ts
+++ jback-angular/src/main/ngapp/src/app/front-page/front-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SessionService } from '../session/session.service';
+
+import { Perspective } from '../session/perspective';
+
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.component.html',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontPageComponent implements OnInit {
 
-  constructor() { }
+  perspective: keyof typeof Perspective;
+
+  constructor(private sessionService: SessionService) { }
 
   ngOnInit(): void {
+    this.sessionService.getSessionInfo()
+    .subscribe(result => {
+      this.perspective = result.perspective;
+    })
   }
 
+  userIsGuest(): boolean {
+    return Perspective[this.perspective] ==  Perspective.GUEST;
+  }
+
+  userIsEnrolling(): boolean {
+    return Perspective[this.perspective] ==  Perspective.ENROLLING;
+  }
+
+  userIsMember(): boolean {
+    return Perspective[this.perspective] ==  Perspective.MEMBER;
+  }
 }
--- jback-angular/src/main/ngapp/src/app/session/session-info.ts
+++ jback-angular/src/main/ngapp/src/app/session/session-info.ts
@@ -1,8 +1,12 @@
+import { Perspective } from './perspective';
+
 export interface SessionInfo {
 
     isAuthenticated: boolean;
 
     principalName: string;
+
+    perspective: keyof typeof Perspective;
 
     firstName: string;
   
