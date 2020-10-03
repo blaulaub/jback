b2397f0d93bb7367ef7dbbd858f9a00cfbc49c06
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-03 04:32:57 +0200

    remove misleading comment

--- jback-spring/jback-core-spring/src/main/java/ch/patchcode/jback/core/CoreConfiguration.java
+++ jback-spring/jback-core-spring/src/main/java/ch/patchcode/jback/core/CoreConfiguration.java
@@ -9,13 +9,6 @@ import ch.patchcode.jback.core.persons.impl.PersonServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-/**
- * Spring configuration that automatically contains all services (and other
- * components) of this package and below through component scan. It does not
- * pre-configure any dependencies (repositories), these need to be configured
- * elsewhere.
- */
 @Configuration
 public class CoreConfiguration {
 
