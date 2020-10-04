ee855f8b9f1c461ffd5a047144d284f31c4e6a19
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-04 13:47:17 +0200

    test session perspective

---
+++ jback-api/src/test/java/ch/patchcode/jback/api/SessionTest.java
@@ -0,0 +1,38 @@
+package ch.patchcode.jback.api;
+
+import ch.patchcode.jback.presentation.Perspective;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ApiTestConfiguration.Apply
+class SessionTest {
+
+    private final MockMvc mvc;
+
+    @Autowired
+    public SessionTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("the initial session perspective is 'GUEST'")
+    void initialPerspectiveIsGuest() throws Exception {
+
+        // act
+        var result = mvc.perform(get("/api/v1/session")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8"));
+
+        // assert
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.perspective").value(Perspective.GUEST.toString()));
+    }
+}
