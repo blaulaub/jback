9611451a80a63eec9d4a6dc71d3e3134b15b5fb6
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-04 13:04:21 +0200

    dropped all tests

--- jback-api/src/test/java/ch/patchcode/jback/api/fakeServices/FakeServicesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ch.patchcode.jback.api.fakeServices;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class FakeServicesConfiguration {
-}
