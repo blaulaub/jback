2aad8e62d6cf7498beb9556e7b05788900b536ad
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-04 13:37:35 +0200

    add empty MockMVC test

---
+++ jback-api/src/test/java/ch/patchcode/jback/api/ApiTestConfiguration.java
@@ -0,0 +1,107 @@
+package ch.patchcode.jback.api;
+
+import ch.patchcode.jback.core.CoreConfiguration;
+import ch.patchcode.jback.jpa.entitiesSpring.JpaEntitiesConfiguration;
+import ch.patchcode.jback.jpa.wrappers.JpaWrapperConfiguration;
+import ch.patchcode.jback.presentation.PresentationConfiguration;
+import ch.patchcode.jback.securitySpring.SecurityConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Properties;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebMvc
+@Import({
+        ApiConfiguration.class,
+        JpaWrapperConfiguration.class,
+        JpaEntitiesConfiguration.class,
+        PresentationConfiguration.class,
+        CoreConfiguration.class,
+        SecurityConfiguration.class,
+        ApiWebSecurityTestConfig.class
+})
+public class ApiTestConfiguration {
+
+    @Bean
+    public DataSource dataSource(Environment env) {
+
+        var dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("spring.datasource.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getRequiredProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
+
+        var entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("ch.patchcode.jback.jpa");
+
+        Properties jpaProperties = new Properties();
+
+        //Configures the used database dialect. This allows Hibernate to create SQL
+        //that is optimized for the used database.
+        jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+
+        //Specifies the action that is invoked to the database when the Hibernate
+        //SessionFactory is created or closed.
+        jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+
+        //Configures the naming strategy that is used when Hibernate creates
+        //new database objects and schema elements
+        jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
+
+        //If the value of this property is true, Hibernate writes all SQL
+        //statements to the console.
+        jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+
+        //If the value of this property is true, Hibernate will format the SQL
+        //that is written to the console.
+        jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager getTransactionManager(EntityManagerFactory entityManagerFactory) {
+
+        var transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @WebMvcTest
+    @ContextConfiguration(classes = {ApiTestConfiguration.class})
+    @ActiveProfiles("test")
+    public @interface Apply {
+    }
+}
