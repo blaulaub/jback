71efce7005ed22ee75f41e80fa984a8caedff4ae
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-04 12:54:31 +0200

    drop tests from main

--- jback-main/src/test/java/ch/patchcode/jback/main/fakes/FakesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ch.patchcode.jback.main.fakes;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class FakesConfiguration {
-}
