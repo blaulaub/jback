b010a686c4d71fc43a793c5aebe1f77bc0fe1c20
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-04 17:01:37 +0200

    add test for session upgrade (disabled)

--- jback-api/src/test/java/ch/patchcode/jback/api/RegistrationTest.java
+++ jback-api/src/test/java/ch/patchcode/jback/api/RegistrationTest.java
@@ -2,11 +2,14 @@ package ch.patchcode.jback.api;
 
 import ch.patchcode.jback.api.registration.PendingRegistrationInfo;
 import ch.patchcode.jback.api.verification.VerificationCode;
+import ch.patchcode.jback.presentation.Perspective;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static ch.patchcode.jback.api.ConstantVerificationCodeProvider.VERIFICATION_CODE;
+import static org.hamcrest.Matchers.equalTo;
 
 @ApiTestConfiguration.Apply
 class RegistrationTest {
@@ -36,7 +39,8 @@ class RegistrationTest {
     }
 
     @Test
-    void postingVerificationCode() throws Exception {
+    @DisplayName("putting correct verification code is ok")
+    void puttingCorrectVerificationCodeIsOk() throws Exception {
 
         // arrange
         var content = Some.initialRegistrationData();
@@ -50,5 +54,24 @@ class RegistrationTest {
 
         // assert
         result.expectStatus().isOk();
+    }
+
+    @Disabled("testing sessions not supported yet, so this test will fail")
+    @Test
+    @DisplayName("after putting correct verification session becomes XXX")
+    void afterPuttingCorrectVerificationSessionBecomesXXX() throws Exception {
+
+        // arrange
+        var content = Some.initialRegistrationData();
+        var info = api.postRegistration(content).andAssumeGoodAndReturn(PendingRegistrationInfo.class);
+        api.putVerificationCode(info.getPendingRegistrationId(), VerificationCode.of(VERIFICATION_CODE)).andAssumeGoodAndReturn();
+
+        // act
+        var result = api.getSession().andReturn();
+
+        // assert
+        result
+                .expectStatus().isOk()
+                .expectBody().jsonPath("$.perspective").value(equalTo(Perspective.ENROLLING.toString()));
     }
 }
