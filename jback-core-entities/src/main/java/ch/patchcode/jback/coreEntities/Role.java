4eae3ad51585c87d1bcf5430ed47304e60552613
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-17 15:14:39 +0200

    set club for organisation

--- jback-core-entities/src/main/java/ch/patchcode/jback/coreEntities/Role.java
+++ jback-core-entities/src/main/java/ch/patchcode/jback/coreEntities/Role.java
@@ -1,15 +1,17 @@
 package ch.patchcode.jback.coreEntities;
 
-import ch.patchcode.jback.secModel.Organisation;
 import ch.patchcode.jback.secModel.Person;
 
 import java.util.List;
 
-public interface Role<TOrganisation extends Organisation>
-        extends ch.patchcode.jback.secModel.Role<TOrganisation, Person, Authority> {
+public interface Role
+        extends ch.patchcode.jback.secModel.Role<Club, Person, Authority> {
 
     @Override
     Person getPerson();
+
+    @Override
+    Club getOrganisation();
 
     @Override
     List<Authority> getPrivileges();
