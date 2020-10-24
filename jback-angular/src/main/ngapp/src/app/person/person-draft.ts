8d5f8161e817f329575311971a2e07b67728dead
Christian Fuchs <cfuchs@patchcode.ch>
2020-10-24 10:22:32 +0200

    remove unused verification code

--- jback-angular/src/main/ngapp/src/app/person/person-draft.ts
+++ jback-angular/src/main/ngapp/src/app/person/person-draft.ts
@@ -9,16 +9,4 @@ export class PersonDraft {
   password: string = null;
 
   address: string[] = [];
-
-  private static notNullOrEmpty(str: string): boolean {
-    return typeof str === 'string' && str !== '';
-  }
-
-  static isValid(draft: PersonDraft): boolean {
-    return PersonDraft.notNullOrEmpty(draft.firstName) &&
-      PersonDraft.notNullOrEmpty(draft.lastName) &&
-      PersonDraft.notNullOrEmpty(draft.username) &&
-      PersonDraft.notNullOrEmpty(draft.password) &&
-      draft.address.every(line => PersonDraft.notNullOrEmpty(line));
-  }
 }
